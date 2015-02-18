985f596baca370900059637efc5adc0a0fd4a300
Ka Tran <kabuky_knight@yahoo.com>
2015-02-18 12:06:44 +0800

    a

--- app/controllers/setting_controller.js
+++ app/controllers/setting_controller.js
@@ -1,5 +1,8 @@
-var Industry       		= require('../../app/models/settings/industry');
-var constants = require('../../app/utils/constants');
+var Industry       	= require('../../app/models/settings/industry');
+var City			= require('../../app/models/city');
+var Country			= require('../../app/models/country');
+var constants 		= require('../../app/utils/constants');
+var async 			= require('async');
 
 var SettingController = function() {
 
@@ -8,21 +11,154 @@ var SettingController = function() {
 module.exports = SettingController;
 
 /**
- * Find page that authenticated user is managing.
+ * Find industries by keyword.
  * 
- * @param  {String} userId - Data submitted from web form.
- * @return [ActionResult]
+ * @param  {String} keyword - user keyword.
+ * @return [Industry]
  */
 SettingController.findIndustryByKeyword = function (keyword, callback) {
 	var re = new RegExp(keyword, 'i');
 
-	Industry.find({ name: { $regex: re }}, function (err, industries) {
+	// Since sup-industries have no parent and we don't want to return them together with sub-industries, so
+	// filter them out by $ne: null
+	Industry.find({ $and: [ {name: { $regex: re }}, {parent: { $ne: null }} ]}, {limit: 1}, function (err, industries) {
 		if (err) {
 			return callback(industries);
 		}
 
 		// Find parent industry if any
-		return callback(industries);
+		var industryObject = {
+			findIndustryParent: function(industry, callbackMap) {
+				var industryJson = {
+					_id: industry._id,
+					name: industry.name,
+					slug: industry.slug,
+					parent: null
+				};
+				
+				// Return sup-industries also
+				if (industry.parent) {
+					Industry.findById(industry.parent, function (err, parent) {
+						if (err) {
+							callbackMap(err);
+						}
+						
+						if (parent) {
+							var parentJson = {
+								_id: parent._id,
+								name: parent.name,
+								slug: parent.slug
+							}
+							industryJson.parent = parentJson;							
+						} 
+						callbackMap(null, industryJson);
+					});
+				} else {
+					callbackMap(null, industryJson);
+				}
+			}
+		}
+		
+		async.map(industries, industryObject.findIndustryParent.bind(industryObject), function (err, callbackResult) {
+			return callback(callbackResult);
+		});
 	});
 };
 
+/**
+ * Find industry by id.
+ * 
+ * @param  {String} id - Industry Id.
+ * @return {Industry}
+ */
+SettingController.findIndustryById = function (id, callback) {
+
+	Industry.findById(id, function (err, industry) {
+		if (err) {
+			return callback(null);
+		}
+		
+		if (!industry) {
+			return callback(null);
+		}
+		
+		var industryJson = {
+			_id: industry._id,
+			name: industry.name,
+			slug: industry.slug,
+			parent: null
+		};
+		
+		if (industry.parent) {
+			Industry.findById(industry.parent, function (err, parent) {
+				if (err) {
+					callbackMap(err);
+				}
+				
+				if (parent) {
+					var parentJson = {
+						_id: parent._id,
+						name: parent.name,
+						slug: parent.slug
+					}
+					industryJson.parent = parentJson;							
+				}
+				return callback(industryJson);
+			});
+		} else {
+			return callback(industryJson);
+		}
+	});
+};
+
+/**
+ * Find cities by keyword.
+ * 
+ * @param  {String} keyword - user keyword.
+ * @return [City]
+ */
+SettingController.findCityByKeyword = function (keyword, callback) {
+	var re = new RegExp(keyword, 'i');
+
+	City.find({name: { $regex: re }}, function (err, cities) {
+		if (err) {
+			return callback(null);
+		}
+
+		// Find country of cities
+		var industryObject = {
+			findIndustryParent: function(industry, callbackMap) {
+				var industryJson = {
+					_id: industry._id,
+					name: industry.name,
+					slug: industry.slug,
+					parent: null
+				};
+				
+				if (industry.parent) {
+					Industry.findById(industry.parent, function (err, parent) {
+						if (err) {
+							callbackMap(err);
+						}
+						
+						if (parent) {
+							var parentJson = {
+								_id: parent._id,
+								name: parent.name,
+								slug: parent.slug
+							}
+							industryJson.parent = parentJson;							
+						} 
+						callbackMap(null, industryJson);
+					});
+				} else {
+					callbackMap(null, industryJson);
+				}
+			}
+		}
+		
+		async.map(industries, industryObject.findIndustryParent.bind(industryObject), function (err, callbackResult) {
+			return callback(callbackResult);
+		});
+	});
+};
